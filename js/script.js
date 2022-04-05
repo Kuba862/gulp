dcd48a5fb68321ac1d24cd045843179dcd9b02ce
jakubrejch <jakubrejch@gmail.com>
2022-04-05 19:13:23 +0200

    use objects in form

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
-const name = document.querySelector('input[name=name]');
-const lastName = document.querySelector('input[name=last_name]');
-const email = document.querySelector('input[name=email]');
+let name = document.querySelector('input[name=name]');
+let lastName = document.querySelector('input[name=last_name]');
+let email = document.querySelector('input[name=email]');
 const action = document.querySelector('.action');
+let age = document.querySelector('input[name=age]');
+let proffession = document.querySelector('input[name="proffession]');
+const secondSection = [...document.querySelectorAll('.details')];
 const data = [];
 
-
-const onClickBtn = (e) => {
-    e.preventDefault();
-    data.push(name.value, lastName.value, email.value);
-    console.log(data);
+hideDetails = () => {
+    for(let i = 0; i < secondSection.length; i++) {
+        secondSection[i].style.display = 'none';
+    }
+}
+showDetails = () => {
+    for(let i = 0; i < secondSection.length; i++) {
+        secondSection[i].style.display = 'inline-block';
+    }
 }
 
-action.addEventListener('click', onClickBtn);
+const person = {
+    name: name.value,
+    lastName: lastName.value,
+    email: email.value,
+    show() {
+        console.log(`I'm ${this.name} ${this.lastName} with email: ${this.email}`);
+    },
+    details: {
+        age: this.age,
+        proffession: this.proffession, 
+        showDetails() {
+            console.log(`I'm ${this.age} years old and work as ${this.proffession}`);
+        }
+    }
+}
+
+name.addEventListener('keyup', () => {
+    person.name = name.value;
+});
+lastName.addEventListener('keyup', () => {
+    person.lastName = name.lastName;
+});
+email.addEventListener('keyup', () => {
+    person.email = email.value;
+});
+
+(person.name == '' && person.lastName == '' && person.email == '') ? hideDetails() : showDetails();
+
+action.addEventListener('click', (e) => {
+    e.preventDefault();
+    (person.name !== '' && person.lastName !== '' && person.email !== '') ? showDetails() : hideDetails();
+});
+
+
+
+// action.addEventListener('click', onClickBtn);
